3fbdbed4ee7f25c69b12c96c882cb22ee88bf3f1
EsthefanyCL <echugnasl17_1@unc.edu.pe>
2021-09-06 21:22:27 -0500

    Boton Colorear

--- myJs/lab05.js
+++ myJs/lab05.js
@@ -27,21 +27,23 @@
             document.getElementById("longitudA").value = " " + logApellido;
 
             //edad 
-          
             let textFechaN = fNaciIngresado.value.split('-');
             let fechaActual = new Date();
-            let fechaNacimiento = new Date(textFechaN);
-            let edad = fechaActual.getFullYear()-fechaNacimiento.getFullYear();
-            let edadmeses = fechaActual.getMonth()-fechaNacimiento.getMonth(); 
+
+            mesAc = fechaActual.getMonth()+1;
+            anioAc = fechaActual.getFullYear();
+            anioFN = textFechaN[0];
+            mesFN = textFechaN[1];
+
+            let edad = anioAc-anioFN;
+            let edadmeses = mesAc-mesFN; 
             if(edadmeses<0){
                 edad = edadA-1;
                 return edad;
             }          
             document.getElementById("miEdad").value =" "+ edad;
 
-            mes = textFechaN[1];
-            
-            switch(mes){
+            switch(mesFN){
                 case "01":  mesLetra="Enero";
                 break;
                 case "02": mesLetra ="Febrero";
@@ -67,13 +69,51 @@
                 case "12": mesLetra ="Diciembre";
                 break;
                 default:
-                    console.log(' er');
-                
+                    console.log(' ');   
             }
-            document.getElementById("mesNacimiento").value = " " + mesLetra;
+            document.getElementById("mesNacimiento").value = " " + mesLetra;   
         }
-    
-           
+
+        //colorearButton.addEventListener('click', colorear, true);
+        var colorearButton = document.getElementById("colorear");
+        colorearButton.onclick = cambiarcolor;
+
+
+        let colorazul1=document.getElementById("azul1");
+        let colorazul2=document.getElementById("azul2");
+        let colorazul3=document.getElementById("azul3");
+        let colorazul4=document.getElementById("azul4");
+        let colorazul5=document.getElementById("azul5");
+
+        let colorrojo1=document.getElementById("rojo1");
+        let colorrojo2=document.getElementById("rojo2");
+        let colorrojo3=document.getElementById("rojo3");
+            
+        let c=0;
+        function cambiarcolor(){ 
+        
+            if(c==0){
+                colorazul1.classList.add("rojo");
+                colorazul2.classList.add("rojo");
+                colorazul3.classList.add("rojo");
+                colorazul4.classList.add("rojo");
+                colorazul5.classList.add("rojo");
+                colorrojo1.classList.add("azul");
+                colorrojo2.classList.add("azul");
+                colorrojo3.classList.add("azul");
+                c=1;
+            }else{
+                colorazul1.classList.remove("rojo");
+                colorazul2.classList.remove("rojo");
+                colorazul3.classList.remove("rojo");
+                colorazul4.classList.remove("rojo");
+                colorazul5.classList.remove("rojo");
+                colorrojo1.classList.remove("azul");
+                colorrojo2.classList.remove("azul");
+                colorrojo3.classList.remove("azul");
+                c=0;
+            }
+        }  
     }
 })();
 
